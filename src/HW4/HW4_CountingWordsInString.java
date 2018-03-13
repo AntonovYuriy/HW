13aacd4c44f350355e7756a132ec2f48c5e71728
Burnwithin <burnwithin@ukr.net>
2018-03-13 16:46:38 +0200

    Finish HW4

--- src/HW4/HW4_CountingWordsInString.java
+++ src/HW4/HW4_CountingWordsInString.java
@@ -4,13 +4,14 @@ public class HW4_CountingWordsInString {
 
 	public static void main(String[] args) {
 
-		String s = "Hello my world!!!";
-//		String s = "Have you tried to read and understand Java’s String format documentation? I have and found it hard to understand. While it does include all the information, the organization leaves something to be desired";
+//		String s = "Hello my world!!!";
+		String s = "Have you tried to read and understand Java’s String format documentation? I have and found it hard to understand. While it does include all the information, the organization leaves something to be desired";
 		System.out.printf("Our input string is %n" + s + "%n");
 		
 		System.out.println("First way - we have " + HowManySpaces(s) + " scaces");
 		System.out.println("First way - we have " + (HowManySpaces(s)+1) + " words");
 	
+		System.out.println("Second way - we have " + HowManyWords(s) + " words");
 	}
 	
 	
@@ -23,8 +24,17 @@ public class HW4_CountingWordsInString {
 				if (st.charAt(i) == ' ') 
 					counter++;
 		return counter;			
-			
-
+	
 	}
+	
 
+	//Second Way
+
+	static int HowManyWords (String st) {
+		int counter = 0;
+		
+		for (String s : st.split(" "))   	counter++;
+		
+		return counter;	
+	}
 }
