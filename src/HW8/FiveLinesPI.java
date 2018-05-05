b4b3553ab2c3877b0a933306f5dfcd2b67ae90e7
Burnwithin <burnwithin@ukr.net>
2018-05-05 11:13:13 +0300

    First symbol replace done

---
+++ src/HW8/FiveLinesPI.java
@@ -0,0 +1,15 @@
+// Вывести на экран 5 строк числа Пи
+//в первой строке 1 знак после запятой, во второй два знака после запятой и так далее
+//java.util.Formatter
+
+
+package HW8;
+
+public class FiveLinesPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
